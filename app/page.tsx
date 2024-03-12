5b8454f33d55027dcce40a64cda050fc52fa8c24
Jesse <jessebeke10@gmail.com>
2024-03-12 22:35:07 +0100

    hero component layout initial set-up

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar"
 
 // this will be the home page
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex flex-col">
       <Navbar />
+      <Hero />
     </main>
   );
 }

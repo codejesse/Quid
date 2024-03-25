a12d6cbee7719ab49c9df4efa9ee121bd6413cf6
Jesse <jessebeke10@gmail.com>
2024-03-25 02:32:46 +0100

    added grid 3 media

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import Image from "next/image";
 import grid_1 from "../../assets/grid-1.svg";
 import grid_2 from "../../assets/grid-2.png";
+import grid_3 from "../../assets/grid-3.png";
 import stats from "../../assets/stats.png";
+import globe from "../../assets/globe.svg";
+
 {
   /* Add padding or margin to bento grids container */
 }
@@ -15,48 +18,62 @@ const About = () => {
           Feel the best experience with our features
         </h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 lg:m-12 p-4 border border-red-400">
-        <div className="bg-gray-100 h-[583px] overflow-hidden rounded-xl p-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 lg:m-12 p-4">
+        <div className="bg-gray-100 h-[583px] overflow-hidden rounded-[30px] p-10">
           <div className="w-10 h-10 bg-[#EB632B] rounded-full text-white">
             icon
           </div>
           <h3 className="w-7/12 text-2xl">
             Create custom online cards with ease.
           </h3>
-          <p className="font-regular text-[#818181]">
+          <p className="font-regular w-7/12 text-[#818181]">
             Our application gives you the power to create custom cards.
           </p>
-          <Image src={grid_1} alt="screens" />
+          <Image className="ml-[-30px]" src={grid_1} alt="screens" />
         </div>
-        <div className="bg-gray-100 overflow-hidden h-[583px] rounded-xl p-5">
+        <div className="bg-gray-100 overflow-hidden h-[583px] rounded-[30px] p-10">
           <div className="w-10 h-10 bg-[#EB632B] rounded-full text-white">
             icon
           </div>
           <h3 className="w-7/12 text-2xl">
             Personalized financial insights and goals.
           </h3>
-          <p className="font-regular text-[#818181]">
+          <p className="font-regular w-7/12 text-[#818181]">
             Create financial goals and have access to your spending habits from
             one place.
           </p>
           <div className="relative">
-            <Image className="absolute inset-y-0 right-0 lg:top-20" src={stats} alt="stats" />
+            <Image
+              className="absolute inset-y-0 right-0 lg:top-20"
+              src={stats}
+              alt="stats"
+            />
           </div>
           <Image src={grid_2} alt="screens" />
         </div>
       </div>
       <div className="p-4 lg:m-8">
-        <div className="bg-gray-100 h-56 rounded-xl p-5">
+        <div className="bg-gray-100 overflow-hidden h-[401px] rounded-[30px] p-10">
           <div className="w-10 h-10 bg-[#EB632B] rounded-full text-white">
             icon
           </div>
           <h3 className="w-7/12 text-2xl">
             Free transfers anywhere around the world
           </h3>
-          <p className="font-regular text-[#818181]">
+          <p className="font-regular  w-5/12 text-[#818181]">
             Experience the freedom of hassle-free transfers with our free
             transfer admin app. Say goodbye to hidden fees and charges.
           </p>
+          <div className="relative">
+            <Image
+              className="absolute z-[999] inset-y-0 right-0"
+              src={grid_3}
+              alt="screens"
+            />
+          </div>
+          <div className="relative">
+            <Image className="absolute inset-y-0 right-0" src={globe} alt="globe" />
+          </div>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-10 lg:m-12 p-4 border border-red-400">

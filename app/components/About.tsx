7997cf761e45d0c6d99f2f35b82bb9ea08eabb73
Jesse <jessebeke10@gmail.com>
2024-03-27 06:23:50 +0100

    added icons to grid

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,6 +6,11 @@ import grid_3 from "../../assets/grid-3.png";
 import stats from "../../assets/stats.png";
 import globe from "../../assets/globe.svg";
 
+//icons
+import Card from "../../assets/icons/Card.svg";
+import analytics from "../../assets/icons/stats.svg";
+import Card_2 from "../../assets/icons/Card2.svg";
+
 {
   /* Add padding or margin to bento grids container */
 }
@@ -21,7 +26,7 @@ const About = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 lg:m-12 p-4">
         <div className="bg-gray-100 h-[583px] overflow-hidden rounded-[30px] p-10">
           <div className="w-10 h-10 bg-[#EB632B] rounded-full text-white">
-            icon
+            <Image className="text-center" width={30} height={30} src={Card} alt="card-icon"/>
           </div>
           <h3 className="w-7/12 text-2xl">
             Create custom online cards with ease.
@@ -33,12 +38,12 @@ const About = () => {
         </div>
         <div className="bg-gray-100 overflow-hidden h-[583px] rounded-[30px] p-10">
           <div className="w-10 h-10 bg-[#EB632B] rounded-full text-white">
-            icon
+            <Image className="text-center" width={30} height={30} src={analytics} alt="stats-icon"/>
           </div>
           <h3 className="w-7/12 text-2xl">
             Personalized financial insights and goals.
           </h3>
-          <p className="font-regular w-7/12 text-[#818181]">
+          <p className="font-regular w-8/12 text-[#818181]">
             Create financial goals and have access to your spending habits from
             one place.
           </p>
@@ -55,7 +60,7 @@ const About = () => {
       <div className="p-4 lg:m-8">
         <div className="bg-gray-100 overflow-hidden h-[401px] rounded-[30px] p-10">
           <div className="w-10 h-10 bg-[#EB632B] rounded-full text-white">
-            icon
+            <Image className="text-center" width={30} height={30} src={Card_2} alt="card-icon"/>
           </div>
           <h3 className="w-7/12 text-2xl">
             Free transfers anywhere around the world
@@ -79,7 +84,9 @@ const About = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-10 lg:m-12 p-4 border border-red-400">
         <div className="bg-gray-100 h-56 rounded-xl">Grid four</div>
         <div className="bg-gray-100 h-56 rounded-xl">Grid five</div>
-        <div className="bg-gray-100 h-56 rounded-xl">Grid six</div>
+        <div className="bg-[#EB632B] h-56 rounded-xl bg-[url('/footer_bg.svg')] bg-right-top bg-contain">
+          <h2 className="text-white text-2xl w-8/12">Check our more product detailed features</h2>
+        </div>
       </div>
     </div>
   );

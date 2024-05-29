34c3e7eade0dff920273cad16f8b7300a86312cb
Jesse <jessebeke10@gmail.com>
2024-05-29 19:56:16 +0100

    feat: add fallback and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex">
-      <h1>Hello world</h1>
+      <h1>Hello world!!</h1>
     </main>
   );
 }
